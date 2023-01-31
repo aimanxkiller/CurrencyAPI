caaa050a3b494f54a1e3f15b2bb1647f9c2afa12
aimanxkiller <aimanxkiller@gmail.com>
2023-01-31 15:14:28 +0800

    Commit

--- app/src/main/java/com/example/currencyapi/MainActivity.kt
+++ app/src/main/java/com/example/currencyapi/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.currencyapi
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -15,11 +18,20 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var myAdapter: MyAdapter
+    lateinit var linearlayoutManager: LinearLayoutManager
+    lateinit var recyclerView: RecyclerView
+
     val BASE_URL = "https://open.er-api.com/v6/latest/"
     lateinit var data:CurrencyRates2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.recyclerviewCur)
+        recyclerView.setHasFixedSize(true)
+        linearlayoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = linearlayoutManager
 
         getCurrencyData()
     }
@@ -41,6 +53,7 @@ class MainActivity : AppCompatActivity() {
         val retrofitData = retrofitBuilder.getData()
 
         retrofitData.enqueue(object : Callback<CurrencyRates2>{
+            @SuppressLint("SuspiciousIndentation")
             override fun onResponse(call: Call<CurrencyRates2>, response: Response<CurrencyRates2>) {
                 data = response.body()!!
 
@@ -53,7 +66,8 @@ class MainActivity : AppCompatActivity() {
                     val splitter = string[index].split("=")
                         Log.e("DataSplit",   "RM 1 value is " + splitter[1] +splitter[0])
                 }
-
+                val myAdapter = MyAdapter(baseContext, data)
+                recyclerView.adapter = myAdapter
             }
 
             override fun onFailure(call: Call<CurrencyRates2>, t: Throwable) {
---
+++ app/src/main/java/com/example/currencyapi/MyAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.currencyapi
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+
+class MyAdapter (val context:Context, val userList:CurrencyRates2):RecyclerView.Adapter<MyAdapter.ViewHolder>(){
+    class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
+        var userID:TextView
+        var title:TextView
+
+        init {
+            userID = itemView.findViewById(R.id.textID)
+            title =  itemView.findViewById(R.id.textTitle)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        var itemView = LayoutInflater.from(context).inflate(R.layout.rows_item,parent,false)
+        return ViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        var string = userList.rates.toString()
+            .replace("(","")
+            .replace(")","")
+            .split(",")
+
+        string.forEachIndexed { index, s ->
+            val splitter = string[index].split("=")
+            Log.e("DataSplit",   "RM 1 value is " + splitter[1] +splitter[0])
+            holder.userID.text = splitter[0]
+            holder.title.text = splitter[1]
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        var string = userList.rates.toString()
+            .replace("(","")
+            .replace(")","")
+            .split(",")
+
+        return string.size
+    }
+
+}
